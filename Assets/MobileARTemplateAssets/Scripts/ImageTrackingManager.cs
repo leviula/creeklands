9a4da0904c77d4ed4ee92f159c5d9876945bf172
leviula <dinglinliu04@gmail.com>
2026-07-03 11:51:40 -0700

    feat: first prototype of image tracking
    created prefab for heron and image tracking manager to add reference images.

---
+++ Assets/MobileARTemplateAssets/Scripts/ImageTrackingManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageTrackingManager : MonoBehaviour
+{
+    public ARTrackedImageManager trackedImageManager;
+    public GameObject prefab;
+
+    private GameObject spawnedObject;
+
+    void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            if (trackedImage.referenceImage.name == "IMG_6009")
+            {
+                spawnedObject = Instantiate(
+                    prefab,
+                    trackedImage.transform.position,
+                    trackedImage.transform.rotation);
+            }
+        }
+    }
+}
